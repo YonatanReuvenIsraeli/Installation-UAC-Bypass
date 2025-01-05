80081395f090bf97b2cb504151c47329729251fb
Yonatan Israeli <yonirisraeli@gmail.com>
2025-01-04 21:52:05 -0500

    Update Installation UAC Bypass.bat

--- Installation UAC Bypass.bat
+++ Installation UAC Bypass.bat
@@ -2,12 +2,20 @@
 setlocal
 title Installation UAC Bypass
 echo Program Name: Installation UAC Bypass
-echo Version: 1.1.8
+echo Version: 1.1.9
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
-echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli
+"%windir%\System32\net.exe" user > nul 2>&1
+if not "%errorlevel%"=="0" goto "InWindowsRecoveryEnvironment"
 goto "Disclaimer"
+
+:"InWindowsRecoveryEnvironment"
+echo.
+echo Please run this batch file from within Windows. Press any key to close this batch file.
+pause > nul 2>&1
+goto "Exit"
 
 :"Disclaimer"
 echo.
